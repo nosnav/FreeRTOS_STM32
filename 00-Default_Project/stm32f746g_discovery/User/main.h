68c23f348c829aa8cd4efd2b694b9ae07edfbec0
thanh.duong.vs <thanh.duong.vs@gmail.com>
2016-05-17 23:57:56 +0700

    update default project stm32f746g discovery

---
+++ 00-Default_Project/stm32f746g_discovery/User/main.h
@@ -0,0 +1,26 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32f7xx_hal.h"
+#include "stm32f7_system.h"
+#include "stm32f7_discovery.h"
+     
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "queue.h"
+#include "semphr.h"
+#include "event_groups.h"
+//#include "stm32f7_delay.h"
+//#include "stm32f7_touch.h"
+//#include "stm32f7_lcd.h" 
+     
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
